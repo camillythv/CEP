95f3fd1f87788d6af89b76bdbe7fd921517e12a2
littleManu13 <161728383+littleManu13@users.noreply.github.com>
2026-03-09 20:46:34 -0300

    feat: Criação do metodo mostrar mensagem

--- CEP.java
+++ CEP.java
@@ -1,5 +1,11 @@
 public class CEP {
     public static void main (String [] args) {
         System.out.println ("hello World");
+        mostrarMensagem();
+    }
+
+    public static void mostrarMensagem() {
+        System.out.println("Programa executado com sucesso!");
     }
 }
+
